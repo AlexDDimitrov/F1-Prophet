c528aa500bf90bd701f28c59a4b0805580ed6588
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-03-01 18:18:32 +0200

    testing, making home page

---
+++ F1Prophet/src/Components/addDrivers.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DisplayDriver from './driver'
+
+function CollectDrivers() {
+    return (
+        <div>
+            <DisplayDriver name = "Max Verstappen"/>
+            <DisplayDriver name = "Charles Leclerc"/>
+            <DisplayDriver name = "Lando Norris"/>
+        </div>
+    )
+}
+
+export default CollectDrivers
---
+++ F1Prophet/src/Components/driver.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DisplayDriver({name}) {
+    return (
+        <div>
+            <h1><strong>{name}</strong></h1>
+        </div>
+    )
+}
+
+export default DisplayDriver
