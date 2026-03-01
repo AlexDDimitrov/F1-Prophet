7d2d8032b123bd4877514e0204008b81d30a23e1
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-03-01 19:19:53 +0200

    addedHomePage component and styles. Links are left

--- F1Prophet/src/App.jsx
+++ F1Prophet/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomePage from './Pages/home-page.jsx'
 
 function App() {
   return (
     <div>
-
+      <HomePage/>
     </div>
   )
 }
--- F1Prophet/src/Pages/home-page.jsx
+++ F1Prophet/src/Pages/home-page.jsx
@@ -1,3 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import './HomePage.css'
+
+function HomePage() {
+    return (
+        <div className='homepage'>
+            <section className='hero'>
+                <div className='hero-content'>
+                    <h1 className='hero-title'>
+                        <span className='title-line1'>F1 Prophet</span>
+                        <span className='title-line2'>Race and Win</span>
+                    </h1>
+                    <p className='hero-subtitle'>
+                        Predict results before qualifying and score points.
+                        
+                    </p>
+                    <div className='hero-buttons'>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+//buttons for login, register, predict, drivers to be done
+
+
+export default HomePage
