a9894c671a7e857c91a59d328829ad0c5613b64f
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-06-02 19:11:42 +0300

    Added to CORS origin for potential JAVA backend.

--- F1Prophet/Backend/app/__init__.py
+++ F1Prophet/Backend/app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     
     CORS(app, resources={
         r"/api/*": {
-            "origins": ["http://localhost:5173", "http://localhost:3000"]
+            "origins": ["http://localhost:5173", f"http://localhost:{app.config['PORT']}", "http://localhost:3000/*"]
         }
     })
     
