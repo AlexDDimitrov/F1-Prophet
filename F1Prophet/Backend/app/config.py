ae46177d71448826d575766c5a92af3d22666cbf
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-06-02 19:08:35 +0300

    Update config to use PORT from environment variables in run.py

--- F1Prophet/Backend/app/config.py
+++ F1Prophet/Backend/app/config.py
@@ -10,3 +10,4 @@ class Config:
     MYSQL_USER = os.getenv('MYSQL_USER')
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
     MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+    PORT = int(os.getenv('PORT', 5000))
