05e33203a9a3093ee96cc3833437339694ef31a6
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-07-12 20:35:55 +0300

    Update SQLAlchemy engine options for improved connection management

--- F1Prophet/Backend/app/config.py
+++ F1Prophet/Backend/app/config.py
@@ -15,8 +15,8 @@ class Config:
     
 
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_recycle": 280,
+        "pool_recycle": 60,
         "pool_pre_ping": True,
-        "pool_size": 10,
-        "max_overflow": 20
+        "pool_size": 5,
+        "max_overflow": 10
     }
